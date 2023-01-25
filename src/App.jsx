d520eac03a6e1dac5fae09e6e243db17c023dc78
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-01-25 10:31:40 +0000

    Minor CSS changes of password output

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"];
+  
+  const [password1, setPassword1] = useState('Password 1')
+  const [password2, setPassword2] = useState('Password 2')
+
+  function passwordGenerator(){
+    let p1 = ''
+    let p2 = ''
+    for (let i=0; i < 15; i++){
+      const random = Math.floor(Math.random() * characters.length)
+      const random2 = Math.floor(Math.random() * characters.length)
+      p1 += characters[random]
+      p2 += characters[random2]
+    }
+    setPassword1(p1)
+    setPassword2(p2)
+  }
+  
+  
 
   return (
-    <div className="App">
-        
+    <div className="app">
+      <h1>Generate a <span><br />random password</span></h1>
+      <p> Never use an insecure password again</p>
+      <button onClick={passwordGenerator}>Generate passwords</button>
+      <div className="flex-container">
+        <div className="output">{password1}</div>
+        <div className="output">{password2}</div>
+      </div>
     </div>
   )
 }
